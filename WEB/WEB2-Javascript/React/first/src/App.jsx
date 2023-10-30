6f044dd849a3db7852d21b78457c0bb7c30fe54f
Choi-jujuyeon <chlwndus1216@naver.com>
2023-10-30 11:30:25 +0900

    react-props-DynamicArray+for -props.title

--- WEB/WEB2-Javascript/React/first/src/App.jsx
+++ WEB/WEB2-Javascript/React/first/src/App.jsx
@@ -3,10 +3,16 @@ import Head from "./components/head";
 import Nav from "./components/nav";
 
 function App() {
+    const topics = [
+        { id: 1, title: "html", body: "html is ..." },
+        { id: 2, title: "css", body: "css is ..." },
+        { id: 3, title: "javascript", body: "javascript is ..." },
+    ];
+
     return (
         <div>
             <Head title="WEB" />
-            <Nav />
+            <Nav topics={topics} />
             <Article title="Welcome" body="Hello, React~!~" />
         </div>
     );
--- WEB/WEB2-Javascript/React/first/src/components/nav.jsx
+++ WEB/WEB2-Javascript/React/first/src/components/nav.jsx
@@ -1,16 +1,15 @@
-const Nav = () => {
+const Nav = (props) => {
+    const lis = [];
+
+    for (let i = 0; i < props.topics.length; i++) {
+        let t = props.topics[i];
+        lis.push(<li>{t.title}</li>);
+    }
     return (
         <nav>
             <ol>
-                <li>
-                    <a href="/read/1">html</a>
-                </li>
-                <li>
-                    <a href="/read/2">css</a>
-                </li>
-                <li>
-                    <a href="/read/3">javascript</a>
-                </li>
+                {/* 원소들을 하나씩 꺼내어 배치시킴 */}
+                {lis}
             </ol>
         </nav>
     );
